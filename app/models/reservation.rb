4616029d6e8a0ac025468db8b22565e8107c47cd
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-12-13 09:37:53 +1100

    added check for seats 0 and under

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -14,7 +14,7 @@ class Reservation < ApplicationRecord
   belongs_to :flight
   belongs_to :user
 
-  validates :seat, :presence => true, format: { with: /\A[A-Z]\d+\z/, message: "format incorrect. Please enter seat number in the correct format, e.g. A1"}
+  validates :seat, :presence => true, format: { with: /\A[A-Z]\d+\z/, message: "format incorrect. Please enter seat number in the correct format, e.g. A1."}
   validates_uniqueness_of :seat, :scope => [:flight_id] # checks if seat is taken
   validate :seat_limits
 
@@ -26,7 +26,10 @@ class Reservation < ApplicationRecord
     plane_cols = self.flight.plane.cols
 
     if row > plane_rows
-      errors.add(:seat, "row doesn't exist. Check the number of rows on the plane for this flight.")
+      errors.add(:seat, "row doesn't exist. Please capitalize the letter or check the number of rows on the plane for this flight.")
+    end
+    if col < 1
+      errors.add(:seat, "column doesn't exist.")
     end
     if col > plane_cols
       errors.add(:seat, "column doesn't exist. Check the number of columns on the plane for this flight.")
