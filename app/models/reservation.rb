f1f70bb576ab0c3fe21211b4225ddd5ec2bf1a26
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-12-13 00:07:12 +1100

    added check if seat out of range

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -14,6 +14,22 @@ class Reservation < ApplicationRecord
   belongs_to :flight
   belongs_to :user
 
-  validates :seat, :presence => true, format: { with: /\A[A-Z]\d+\z/, message: "- Please enter seat number in the correct format, e.g. A1"}
-  validates_uniqueness_of :seat, :scope => [:flight_id]
+  validates :seat, :presence => true, format: { with: /\A[A-Z]\d+\z/, message: "format incorrect. Please enter seat number in the correct format, e.g. A1"}
+  validates_uniqueness_of :seat, :scope => [:flight_id] # checks if seat is taken
+  validate :seat_limits
+
+  private
+  def seat_limits
+    row = seat[0].ord - 64
+    col = seat[1..seat.size].to_i
+    plane_rows = self.flight.plane.rows
+    plane_cols = self.flight.plane.cols
+
+    if row > plane_rows
+      errors.add(:seat, "row doesn't exist. Check the number of rows on the plane for this flight.")
+    end
+    if col > plane_cols
+      errors.add(:seat, "column doesn't exist. Check the number of columns on the plane for this flight.")
+    end
+  end
 end
