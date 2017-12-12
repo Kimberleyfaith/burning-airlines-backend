cbd40d41c796e8c65fc9ba69cdc4b8cddf1fdf45
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-12-12 18:13:59 +1100

    validations and whitelist for keys on plane and flights

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -15,4 +15,9 @@
 class Flight < ApplicationRecord
   belongs_to :plane
   has_many :reservations
+
+  validates :number, :presence => true
+  validates :origin, :presence => true
+  validates :destination, :presence => true
+  validates :date, :presence => true
 end
--- app/models/plane.rb
+++ app/models/plane.rb
@@ -12,4 +12,8 @@
 
 class Plane < ApplicationRecord
   has_many :flights
+
+  validates :name, :presence => true, :uniqueness => true
+  validates :rows, :presence => true
+  validates :cols, :presence => true
 end
