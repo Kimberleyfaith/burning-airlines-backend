e736819fdc6b58a26e2d362cc014e618c3f7e65a
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-12-13 11:11:35 +1100

    fixed nil error in validate seat

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -20,19 +20,19 @@ class Reservation < ApplicationRecord
 
   private
   def seat_limits
-    row = seat[0].ord - 64
-    col = seat[1..seat.size].to_i
-    plane_rows = self.flight.plane.rows
-    plane_cols = self.flight.plane.cols
+    unless flight_id == nil
+      row = seat[0].ord - 64
+      col = seat[1..seat.size].to_i
+      plane_rows = self.flight.plane.rows
+      plane_cols = self.flight.plane.cols
 
-    if row > plane_rows
-      errors.add(:seat, "row doesn't exist. Please capitalize the letter or check the number of rows on the plane for this flight.")
-    end
-    if col < 1
-      errors.add(:seat, "column doesn't exist.")
-    end
-    if col > plane_cols
-      errors.add(:seat, "column doesn't exist. Check the number of columns on the plane for this flight.")
+      if row > plane_rows
+        errors.add(:seat, "row doesn't exist. Please capitalize the letter or check the number of rows on the plane for this flight.")
+      elsif col < 1
+        errors.add(:seat, "column doesn't exist.")
+      elsif col > plane_cols
+        errors.add(:seat, "column doesn't exist. Check the number of columns on the plane for this flight.")
+      end
     end
   end
 end
